2501f94dea0ab2acf921d0e7dbb100152382e452
sneeshakp <68599657+sneeshakp@users.noreply.github.com>
2020-08-09 09:42:43 +0530

    Delete compare_monitor_data.cpp

--- compare_monitor_data.cpp
+++
@@ -1,8 +0,0 @@
-#include <compare_monitor_data.h>
-
-void compare_data(monitor_data data_index,float * received_data ,float upperlimit)
-{
-	if(*received_data > upperlimit)
-		alert[data_index].status = TRUE;
-
-}
